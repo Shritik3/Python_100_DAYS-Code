a8354f47fa3d395103708cd56d2815e3cab77f8a
Shritik <shritikraj@hotmail.com>
2022-06-11 11:37:09 +0530

    Completed DAY-7

--- DAY-6/function.py
+++ DAY-6/function.py
@@ -8,7 +8,7 @@ def my_function():
 
 my_function()
 
-
+# Reeborg Levels
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
 # def turn_around():
 #     turn_left()
---
+++ DAY-7/hangman.py
@@ -0,0 +1,87 @@
+#Step 1 
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word=random.choice(word_list)
+print(chosen_word)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess=input("Guess a letter:\n").lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+x=len(chosen_word)
+for i in range(x):
+  if guess==chosen_word[i]:
+    print('Letter found')
+  else:
+    print('Letter not found')
+  
+
+
+
+#Step 2
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display=[]
+for i in chosen_word:
+  display.append("_")
+print(f"{display}")
+guess = input("Guess a letter: ").lower()
+
+
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+x=len(chosen_word)
+for letter in range(x):
+    if chosen_word[letter] == guess:
+        display[letter]=guess
+    else:
+        display[letter]=display[letter]
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+print(display)
+
+
+
+#Step 3
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+bool=False
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+  
+while bool==False:
+  guess = input("Guess a letter: ").lower()
+  
+  #Check guessed letter
+  for position in range(word_length):
+      letter = chosen_word[position]
+      # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+      if letter == guess:
+          display[position] = letter
+  
+  print(display)
+  if not "_" in display:
+    bool=True
+    print('You win')
